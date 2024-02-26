1678eab638bff3dd89010572e713cad932488a5e
riyAdHmobin <riyadhmobin@gmail.com>
2024-02-26 17:08:56 +0600

    category

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function Ramsey\Uuid\v1;
+
+class CategoryController extends Controller
+{
+    
+    public function index(){
+        
+    }
+
+    public function create(){
+        return view('admin.category.create');
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories',
+        ]);
+
+        if($validator->passes()){
+
+        } else{
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->error()
+            ]);
+        }
+
+    }
+
+    public function edit(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function destroy(){
+        
+    }
+
+}
